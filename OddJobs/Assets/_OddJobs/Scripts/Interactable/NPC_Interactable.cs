cfc51e91f0f1ab0e17111a8baba9b56d6980ac15
Elias Willerup <38775178+ewillerup@users.noreply.github.com>
2024-12-21 22:01:53 -0700

    improved default npc prefab

--- OddJobs/Assets/_OddJobs/Scripts/Interactable/NPC_Interactable.cs
+++ OddJobs/Assets/_OddJobs/Scripts/Interactable/NPC_Interactable.cs
@@ -12,6 +12,7 @@ using Unity.VisualScripting;
 
 public class NPC_Interactable : Interactable
 {
+    [SerializeField] private bool useDefaultNPCPromptMessage = true;
 
     public ConversationManager conversationManager;
     NPCConversation conversation;
@@ -28,6 +29,8 @@ public class NPC_Interactable : Interactable
     
     void Start()
     {
+        if (useDefaultNPCPromptMessage) promptMessage = "Talk to " + gameObject.name;
+
         conversationManager = GetComponentInChildren<ConversationManager>();
         conversation = GetComponent<NPCConversation>();
         npcCollider = GetComponent<Collider>();
