37a34bc6f1d8d87f61ffd9ce9ca71d6260fde3ef
Alan <77310193+WondrousNights@users.noreply.github.com>
2024-12-29 11:31:33 -0600

    He be walking, but never straight

--- OddJobs/Assets/_OddJobs/Scripts/ActiveRagdollController.cs
+++ OddJobs/Assets/_OddJobs/Scripts/ActiveRagdollController.cs
@@ -3,76 +3,27 @@ using UnityEngine;
 public class ActiveRagdollController : MonoBehaviour
 {
 
-    [Header("RAGDOLL Joints")]
-
-    [SerializeField] ConfigurableJoint ragdollSpine1;
-    [SerializeField] ConfigurableJoint ragdollSpine2;
-    [SerializeField] ConfigurableJoint ragdollChest;
-    [SerializeField] ConfigurableJoint ragdollLeftShoulder;
-    [SerializeField] ConfigurableJoint ragdollLeftElbow;
-    [SerializeField] ConfigurableJoint ragdollLeftHand;
-
-    [SerializeField] ConfigurableJoint ragdollRightShoulder;
-    [SerializeField] ConfigurableJoint ragdollRightElbow;
-    [SerializeField] ConfigurableJoint ragdollRightHand;
-    [SerializeField] ConfigurableJoint ragdollNeck;
-    [SerializeField] ConfigurableJoint ragdollHead;
-    [SerializeField] ConfigurableJoint ragdollLeftThigh;
-    [SerializeField] ConfigurableJoint ragdollLeftShin;
-    [SerializeField] ConfigurableJoint ragdollLeftFoot;
-
-    [SerializeField] ConfigurableJoint ragdollRightThigh;
-    [SerializeField] ConfigurableJoint ragdollRightShin;
-    [SerializeField] ConfigurableJoint ragdollRightFoot;
-
-
-    [Header("Animator Joints")]
-    [SerializeField] Transform animatorSpine1;
-    [SerializeField] Transform animatorSpine2;
-    [SerializeField] Transform animatorChest;
-    [SerializeField] Transform animatorLeftShoulder;
-    [SerializeField] Transform animatorLeftElbow;
-    [SerializeField] Transform animatorLeftHand;
-
-    [SerializeField] Transform animatorRightShoulder;
-    [SerializeField] Transform animatorRightElbow;
-    [SerializeField] Transform animatorRightHand;
-    [SerializeField] Transform animatorNeck;
-    [SerializeField] Transform animatorHead;
-    [SerializeField] Transform animatorLeftThigh;
-    [SerializeField] Transform animatorLeftShin;
-    [SerializeField] Transform animatorLeftFoot;
-
-    [SerializeField] Transform animatorRightThigh;
-    [SerializeField] Transform animatorRightShin;
-    [SerializeField] Transform animatorRightFoot;
+    [SerializeField] Rigidbody hips;
+    [SerializeField] Rigidbody leftShin;
+    [SerializeField] Rigidbody rightShin;
+    [SerializeField] float speed;
+    [SerializeField] float constantUpwardForce;
+    [SerializeField] float constantDownwardForce;
 
 
     void Update()
     {
-        CopyMotion();
+       hips.AddForce(hips.transform.up * constantUpwardForce);
+
+       hips.AddForce(hips.transform.forward * speed);
+       
+
+       leftShin.AddForce(leftShin.transform.up * constantDownwardForce);
+       rightShin.AddForce(rightShin.transform.up * constantDownwardForce);
         
     }
     
 
 
-    void CopyMotion()
-    {
-        ragdollSpine1.targetRotation = animatorSpine1.rotation;
-        ragdollSpine2.targetRotation = animatorSpine2.rotation;
-        ragdollChest.targetRotation = animatorChest.rotation;
-        ragdollLeftShoulder.targetRotation = animatorLeftShoulder.rotation;
-        ragdollLeftElbow.targetRotation = animatorLeftElbow.rotation;
-        ragdollLeftHand.targetRotation = animatorLeftHand.rotation;
-        ragdollRightShoulder.targetRotation = animatorRightShoulder.rotation;
-        ragdollRightElbow.targetRotation = animatorRightElbow.rotation;
-        ragdollRightHand.targetRotation = animatorRightHand.rotation;
-        ragdollLeftThigh.targetRotation = animatorLeftThigh.rotation;
-        ragdollLeftShin.targetRotation = animatorLeftShin.rotation;
-        ragdollLeftFoot.targetRotation = animatorLeftFoot.rotation;
-        ragdollRightThigh.targetRotation = animatorRightThigh.rotation;
-        ragdollRightShin.targetRotation = animatorRightShin.rotation;
-        ragdollRightFoot.targetRotation = animatorRightFoot.rotation;
-
-    }
+  
 }
